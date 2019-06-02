8517026fc75f6d4b211a2f6859e570a2e7038b47
Chris Carr <christocarr@outlook.com>
2019-06-02 12:38:29 +0100

    Add basic layout styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,33 @@ import headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Christopher Carr</h1>
-      <ul>
-        <li><Link to="about">About</Link></li>
-        <li><Link to="projects">Projects</Link></li>
-        <li><Link to="contact">Contact</Link></li>
-        <li><Link to="blog">Blog</Link></li>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title}>Christopher Carr</Link>
+      </h1>
+      <ul className={headerStyles.navList}>
+        <li>
+          <Link to="about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" className={headerStyles.navItem}
+          activeClassName={headerStyles.activeNavItem}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" className={headerStyles.navItem}
+          activeClassName={headerStyles.activeNavItem}>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="blog" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
+            Blog
+          </Link>
+        </li>
       </ul>
     </header>
   )

c0e12ca1214150b3c394a401db0c91d14e7eb0e6
Chris Carr <christocarr@outlook.com>
2019-06-03 22:44:31 +0100

    Add graphql data to create post list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const Blog = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <h2>Blog Page</h2>
-      <p>This is the blog page</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li>
+              <h3><Link to={`/blog/${edge.node.fields.slug}`}>{edge.node.frontmatter.title}</Link></h3>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
+      
     </Layout>
   )
 }

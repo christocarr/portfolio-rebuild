1e8ef93ddd8208abaf467787bd932c7870bc2585
Chris Carr <christocarr@outlook.com>
2019-06-03 18:49:07 +0100

    Add metadata data to jsx

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, qraphql, useStaticQuery } from 'gatsby'
 
 import headerStyles from './header.module.scss'
 
 const Header = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <header className={headerStyles.header}>
       <h1>
         <Link className={headerStyles.title}>Christopher Carr</Link>
       </h1>
+      <p>{data.site.siteMetadata.title}</p>
       <ul className={headerStyles.navList}>
         <li>
           <Link to="about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>

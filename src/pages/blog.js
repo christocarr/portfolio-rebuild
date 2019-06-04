42d0660bd502671ce23448d3ba65f79aa0a4f346
Chris Carr <christocarr@outlook.com>
2019-06-04 20:24:32 +0100

    Add class names

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
 
 const Blog = () => {
 
@@ -25,12 +26,13 @@ const Blog = () => {
 
   return (
     <Layout>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
-            <li>
-              <h3><Link to={`/blog/${edge.node.fields.slug}`}>{edge.node.frontmatter.title}</Link></h3>
-              <p>{edge.node.frontmatter.date}</p>
+            <li className={blogStyles.post}>
+              <h3>{edge.node.frontmatter.title}</h3>
+              <p className={blogStyles.date}>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.fields.slug}`}>read more</Link>
             </li>
           )
         })}

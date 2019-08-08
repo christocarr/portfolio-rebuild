8c7c9eabe88a4fd47cc3a8e4909183948425f218
Chris Carr <christocarr@outlook.com>
2019-08-08 18:16:05 +0100

    Fix typos

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,13 +10,13 @@ const About = () => {
       <Head title="About" />
       <h2>About</h2>
       <h3>The Short</h3>
-      A web designer and developer working in London, UK. I specialize in frontend developent. 
+      <p>I'm web designer and developer working in London, UK. I specialize in frontend development.</p>
       <h3>The Long</h3>
       <p>Born in a little city called Pretoria in South Africa I came to the big old city of London to find better opportunities
-      and after a little while, wondering the unkown and hazy, I found myself in the world of web delopement. Since then I have
+      and after a little while, wondering the unknown and hazy, I found myself in the world of web development. Since then I have
       not looked back.</p>
-      <p>Right now I develop with React and the JAMStack, in the long term I plan to become a fullstack developer, but I'm open to
-      learning other tech to keep myself open to job oppurtunities.</p>
+      <p>Right now I develop with React and the JAMStack(a fullstack of sorts) and in the long term, if not already, I plan to become a fullstack developer. I'm open minded to
+      learning new tech to keep myself open to job opportunities.</p>
       <p>Please <Link to="contact">contact me</Link></p>
     </Layout>
 

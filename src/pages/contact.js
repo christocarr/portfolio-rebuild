c86ab734f40cf6dc9e76041083d740ad5ba18c22
christocarr <christocarr@outlook.com>
2019-08-15 19:04:33 +0100

    Add social media profile links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,9 @@ const Contact = () => {
       <Head title="Contact "/>
       <h2>Contact Page</h2>
       <p>What is a website without a contact form?</p>
-      <p>Reach out to me on twitter<a href="https://twitter.com/ChrisofCarr">@ChrisofCarr</a></p>
+      <p>Reach out to me on twitter <a href="https://twitter.com/ChrisofCarr">@ChrisofCarr</a></p>
+      <p>Connect with me on <a href="https://www.linkedin.com/in/chris-carr-93288820">LinkedIn</a></p>
+      <p>My repos on <a href="https://github.com/christocarr">GitHub</a></p>
     </Layout>
   )
 }

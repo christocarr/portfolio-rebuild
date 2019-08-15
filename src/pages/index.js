313164290634c338fc87ed05b8fa45ea1a9d3819
christocarr <christocarr@outlook.com>
2019-08-15 18:27:31 +0100

    Fix typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const Index = () => {
     <Layout>
       <Head title="Home"/>
       <h2>Welcome!</h2>
-      <h3>I'm Chris, a web developer living in London</h3>
+      <h3>I'm Chris, a web developer living in London.</h3>
       <p>Need me to build your next website or app? 
       <Link to="contact">Contact me.</Link></p>
     </Layout>

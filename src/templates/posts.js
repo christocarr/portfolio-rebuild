16028b6d72cfa9a66cf8c7cfefcc68968a6d9269
Chris Carr <christocarr@outlook.com>
2019-08-19 19:19:34 +0100

    Add image alt text and credit

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Disqus from 'gatsby-plugin-disqus'
 
 import Layout from '../components/layout'
@@ -9,26 +10,38 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: {slug: { eq: $slug } }) {
       id
+      html
       frontmatter {
         title
         date
+        alt
+        credit
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
-      html
     }
   }
 `
 
 const Post = (props) => {
+  const postData = props.data.markdownRemark.frontmatter
   return (
     <Layout>
-      <Head title={props.data.markdownRemark.frontmatter.title} />
-      <h2>{props.data.markdownRemark.frontmatter.title}</h2>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <Head title={postData.title} />
+      <h2>{postData.title}</h2>
+      <p>{postData.date}</p>
+      <Img fluid={postData.image.childImageSharp.fluid} alt={postData.alt} />
+      <p>{postData.credit}</p>
       <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}>
       </div>
       <Disqus
         identifier={props.data.markdownRemark.id}
-        title={props.data.markdownRemark.frontmatter.title}
+        title={postData.title}
       />
     </Layout>
   )

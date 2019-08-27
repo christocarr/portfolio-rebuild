6c62fd7c9f5fdadaf0a237656ad9939c03710f3f
Chris Carr <christocarr@outlook.com>
2019-08-27 19:40:33 +0100

    Add basic validation

--- src/components/form.js
+++ src/components/form.js
@@ -6,11 +6,11 @@ const Form = () => {
   return (
     <form method="post" action="https://formspree.io/chris.carr.dev@gmail.com">
       <label htmlFor="name">Name</label>
-      <input id="name" name="name"></input>
+      <input id="name" name="name" required/>
       <label htmlFor="email">Email</label>
-      <input id="email" name="email"></input>
+      <input id="email" name="email" type="email" required/>
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" />
+      <textarea id="message" name="message" required/>
       <input type="text" name="_gotcha" className={formStyles.gotcha} />
       <button type="submit">Send</button>
     </form>

824780041ef3474c1ac88be8e47be86a50539078
Chris Carr <christocarr@outlook.com>
2019-11-19 18:15:53 +0000

    Add to about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About = () => {
       <Head title="About" />
       <h2>About me</h2>
       <h3>The Short</h3>
-      <p>I'm an aspiring web designer and developer working in London, UK. I specialize in frontend development.</p>
+      <p>I'm an aspiring web designer and developer working in London, UK. I specialize in frontend development and love creating great user interactions.</p>
       <h3>The Long</h3>
       <p>Born in a little city called Pretoria in South Africa I came to the big old city of London to find better opportunities. Too comfortable with what was then great work but was literally killing me physically and I didn't see it as a chosen career path. I needed something else, something that I'll enjoy without breaking my back.</p>
       <p>That’s when I looked further into web development as I frustratingly enjoyed building my photography website a few years before. To find out if I still enjoyed building things for the web, I then took part in Udacity's Google Developer Challenge Scholarship which I completed and then received and successfully completed a full six month Google Developer Nanodegree Scholarship. Since I have not looked back and enjoy every minute.</p>

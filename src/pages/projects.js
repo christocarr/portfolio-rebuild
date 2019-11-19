83a9640739da380260f4c55dc785cd90fa129478
Chris Carr <christocarr@outlook.com>
2019-11-19 20:53:18 +0000

    Add project description

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,21 +2,26 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
+import ProjectStyles from './project.module.scss'
+import SibiSearch from '../images/sibi-search.jpg'
 
 const Projects = () => {
   return (
     <Layout>
       <Head title="Projects" />
       <h2>Projects</h2>
-      <p>Future projects will appear on this page.</p>
-      <p>Past projects <a href="https://iamchriscarr.com/projects.html">here</a> on my old website.</p>
       <h3>SIBI Activity Search</h3>
       <h4>The Problem</h4>
       <p>CVS Brent had a spreadsheet with over 1600 records they were using in their Social Isolation in Brent Initiative (SIBI). 
         The initiative allows health care professionals help people that are isolated due to health find social activities they might be interested in.
         Searching for a particular activity on a spreadsheet that contains 1600 entries is a tiresome process.</p>
       <h4>The Solution</h4>
-      <p>A React app that pulls in the data from the spreadsheet and allows the user to search for activities within an area of the patient's postcode.</p>
+      <p>A responsive React web app that pulls in the spreadsheet data and allows the user to search for activities within an area of the patient's postcode.</p>
+      <div className={ProjectStyles.imgContainer}>
+        <img src={SibiSearch} alt="sibi activity search" />
+        <p>Visit the project <a href="https://activity-search.netlify.com/">here</a></p>
+      </div>
+      <p>More projects <a href="https://iamchriscarr.com/projects.html">here</a> on my old website.</p>
     </Layout>
   )
 }

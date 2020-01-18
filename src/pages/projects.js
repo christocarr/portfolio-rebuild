2ea37b2848ba10b9f77425a39f8b288aebb2c0f0
Chris Carr <christocarr@outlook.com>
2020-01-18 11:59:57 +0000

    Add project container div

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,16 +10,21 @@ export default ({ data }) => (
   <Layout>
     <Head title="Projects" />
     <h2>Projects</h2>
-    <h3>SIBI Activity Search</h3>
-    <h4>The Problem</h4>
-    <p>CVS Brent, an organization that provides community support, has a spreadsheet with over 1600 records they were using in their Social Isolation in Brent Initiative (SIBI). 
-      The initiative allows health care professionals help people that are isolated due to health problems, find social activities they might be interested in.
-      Searching for a particular activity on a spreadsheet that contains 1600 entries is a tiresome process.</p>
-    <h4>The Solution</h4>
-    <p>A responsive React web app that pulls in the spreadsheet data and allows the user to search for activities within an area of the patient's postcode.</p>
-    <div className={ProjectStyles.imgContainer}>
-      <Img fluid={data.file.childImageSharp.fluid} alt="sibi activity search" />
-      <p>Visit the project <a href="https://activity-search.netlify.com/">here</a></p>
+    <div className="project-container">
+      <h3>SIBI Activity Search</h3>
+      <h4>The Problem</h4>
+      <p>CVS Brent, an organization that provides community support, has a spreadsheet with over 1600 records they were using in their Social Isolation in Brent Initiative (SIBI). 
+        The initiative allows health care professionals help people that are isolated due to health problems, find social activities they might be interested in.
+        Searching for a particular activity on a spreadsheet that contains 1600 entries is a tiresome process.</p>
+      <h4>The Solution</h4>
+      <p>A responsive React web app that pulls in the spreadsheet data and allows the user to search for activities within an area of the patient's postcode.</p>
+      <div className={ProjectStyles.imgContainer}>
+        <Img fluid={data.file.childImageSharp.fluid} alt="sibi activity search" />
+        <p>Visit the project <a href="https://activity-search.netlify.com/">here</a></p>
+      </div>
+    </div>
+    <div className="project-container">
+      <h3>SIBI Activity Search</h3>
     </div>
   </Layout>
 )

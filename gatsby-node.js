407aff811d2372e4d42fb072e7e85a9980fd7d71
Chris Carr <christocarr@outlook.com>
2020-01-18 15:58:09 +0000

    Add condition to check for post type

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,12 +17,16 @@ module.exports.onCreateNode = ({ node, actions }) => {
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const postTemplate = path.resolve('./src/templates/posts.js');
+  const projectTemplate = path.resolve('./src/templates/project-posts.js')
 
   const res = await graphql(`
     query {
       allMarkdownRemark {
         edges {
           node {
+            frontmatter {
+              posttype
+            }
             fields {
               slug
             }
@@ -33,12 +37,23 @@ module.exports.createPages = async ({ graphql, actions }) => {
   `)
   
   res.data.allMarkdownRemark.edges.forEach(edge => {
-    createPage({
-      component: postTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
-      context: {
-        slug: edge.node.fields.slug
-      }
-    })
-  });
+    if (edge.node.frontmatter.posttype === 'project') {
+      createPage({
+        path: `/projects/${edge.node.fields.slug}`,
+        component: projectTemplate,
+        context: {
+          slug: edge.node.fields.slug
+        }
+      })
+    } else {
+      createPage({
+        component: postTemplate,
+        path: `/blog/${edge.node.fields.slug}`,
+        context: {
+          slug: edge.node.fields.slug
+        }
+      })
+    }
+  })
 }
+

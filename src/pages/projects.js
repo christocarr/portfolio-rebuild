2a85ef069de29fe90a7c07c84dac020f0333c208
Chris Carr <christocarr@outlook.com>
2020-01-22 21:06:56 +0000

    Remove frontmatter

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,7 +17,6 @@ const Projects = () => {
           node {
             frontmatter {
               title
-              projectnumber
               image {
                 childImageSharp {
                   fluid (maxWidth: 300) {

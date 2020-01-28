b70d3e630ca96651b3427e085e7e3822da11e36b
Chris Carr <christocarr@outlook.com>
2020-01-28 08:59:56 +0000

    Add form container

--- src/components/form.js
+++ src/components/form.js
@@ -4,16 +4,18 @@ import formStyles from './form.module.scss'
 
 const Form = () => {
   return (
-    <form method="post" action="https://formspree.io/chris.carr.dev@gmail.com">
-      <label htmlFor="name">Name</label>
-      <input id="name" name="name" required/>
-      <label htmlFor="email">Email</label>
-      <input id="email" name="email" type="email" required/>
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" required/>
-      <input type="text" name="_gotcha" className={formStyles.gotcha} />
-      <button type="submit">Send</button>
-    </form>
+    <div className={formStyles.formContainer}>
+      <form method="post" action="https://formspree.io/chris.carr.dev@gmail.com">
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" required/>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" required/>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" required/>
+        <input type="text" name="_gotcha" className={formStyles.gotcha} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
   )
 }
 

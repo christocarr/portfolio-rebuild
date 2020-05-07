079c1f074732403ae6fd561f22c3e4996bc9dde5
Chris Carr <christocarr@outlook.com>
2020-05-07 13:41:26 +0100

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,10 +9,11 @@ const About = () => {
       <Head title="About" />
       <h2>About me</h2>
       <h3>The Short</h3>
-      <p>I'm an aspiring web designer and developer working in London, UK. I specialize in frontend development and love creating great user interactions.</p>
+      <p>I'm an aspiring web developer working in London, UK. I specialize in frontend development and love creating great user interactions and things for the web.</p>
       <h3>The Long</h3>
-      <p>I looked into web development as career when I built my photography website by teaching myself the basics of HTML and CSS. To find out if I still enjoyed building things for the web, I took part in Udacity's Google Developer Challenge Scholarship which I completed and then received and successfully completed a full six month Google Developer Nanodegree Scholarship. Since I have not looked back and enjoy every minute.</p>
-      <p>During the day I work as a compliance coordinator and in my spare time I create and build websites and apps. Right now I enjoy developing using React and the JAMStack(a fullstack of sorts) and in the long term, if not already, I plan to become a fullstack developer. I'm open minded to
+      <p>I looked into web development as career when I built my photography website by teaching myself the basics of HTML and CSS. To find out if I still enjoyed building things for the web, I took part in Udacity's Google Developer Challenge Scholarship which I completed and then received and successfully completed a full six month Google Developer Nanodegree Scholarship. 
+        Since I have not looked back and enjoy every minute.</p>
+      <p>During the day I work as a compliance coordinator and in my spare time I create and build websites and apps and working on university curriculum. Right now I enjoy developing using React and the JAMStack(a fullstack of sorts) and in the long term, if not already, I plan to become a fullstack developer. I'm open minded to
       learning new tech to keep myself open to job opportunities.</p>
     </Layout>
 
